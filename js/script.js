c03b526b4806636fd8a87fec504297827d1941cd
Aline <aline.isilvadamiao@gmail.com>
2023-03-29 11:48:50 -0300

    Galeria de imagens

--- js/script.js
+++ js/script.js
@@ -34,7 +34,7 @@ function ativarPergunta(event) {
     const pergunta = event.currentTarget;
     const controls = pergunta.getAttribute('aria-controls');
     const resposta = document.getElementById(controls);
-    
+
     resposta.classList.toggle('ativa');
     const ativa = resposta.classList.contains('ativa');
     pergunta.setAttribute('aria-expanded', 'true');
@@ -47,3 +47,21 @@ function eventosPerguntas(pergunta) {
 
 perguntas.forEach(eventosPerguntas);
 console.log(perguntas);
+
+//Galeria
+
+const galeria = document.querySelectorAll('.bike-img img');
+const galeriaContainer = document.querySelector('.bike-img');
+
+function trocarImagem(event) {
+    const img = event.currentTarget;
+    const media = matchMedia('(min-width: 1000px)');
+    console.log(media)
+    galeriaContainer.prepend(img)
+}
+
+function eventosGaleria(img) {
+    img.addEventListener('click', trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
